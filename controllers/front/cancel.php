52dbe37c1432075395ec8bbbab359cf54d833f3d
BeltBuckleBill <rubenventer853@gmail.com>
2025-10-24 12:24:33 +0200

    Create cancel.php

---
+++ controllers/front/cancel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * cancel.php
+ *
+ * Copyright (c) 2025 Linuxweb (Pty) Ltd
+ * You (being anyone who is not Linuxweb (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
+ * Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
+ * 
+ * @author     Ruben Venter (ruben@linuxweb.co.za)
+ * @version    1.0.0
+ * @date       23/10/2025
+ *
+ * @link       https://github.com/Linuxweb/Payfast-ThirtyBees/
+ */
+class PayfastCancelModuleFrontController extends ModuleFrontController
+{
+    public function initContent()
+    {
+        parent::initContent();
+        $this->setTemplate('cancel.tpl');
+    }
+}
