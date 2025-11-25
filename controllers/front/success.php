d057df9234f7f1fc33b3683dd0c72addaafe40a4
BeltBuckleBill <rubenventer853@gmail.com>
2025-11-25 10:13:15 +0200

    Update success.php
    Deleted some comments :P

--- controllers/front/success.php
+++ controllers/front/success.php
@@ -13,44 +13,6 @@
  * @link       https://github.com/Linuxweb/Payfast-ThirtyBees/
  */
 
-// class PayfastSuccessModuleFrontController extends ModuleFrontController
-// {
-//     public $ssl = true;
-
-//     public function initContent()
-//     {
-//         parent::initContent();
-
-//         $cart_id = (int)Tools::getValue('id_cart');
-//         $cart = new Cart($cart_id);
-//         $customer = new Customer($cart->id_customer);
-
-//         if (!$cart->id || !$customer->id) {
-//             Tools::redirect($this->context->link->getPageLink('order', true, null, 'step=1'));
-//         }
-
-//         $order_id = Order::getOrderByCartId($cart->id);
-//         $order = $order_id ? new Order($order_id) : null;
-
-//         if ($order && (int)$order->current_state === (int)Configuration::get('PS_OS_PAYMENT')) {
-//             // Payment accepted
-//             $this->context->smarty->assign([
-//                 'order_reference' => $order->reference,
-//                 'order_total' => Tools::displayPrice($cart->getOrderTotal(true, Cart::BOTH), $this->context->currency),
-//                 'order_link' => '../../order-history',
-//             ]);
-//             $this->setTemplate('success.tpl');
-//         } else {
-//             // Payment not yet validated
-//             $this->context->smarty->assign([
-//                 'cart_id' => $cart->id,
-//                 'retry_link' => '../../order',
-//             ]);
-//             $this->setTemplate('failure.tpl');
-//         }
-//     }
-// }
-
 class PayfastSuccessModuleFrontController extends ModuleFrontController
 {
     public $ssl = true;
